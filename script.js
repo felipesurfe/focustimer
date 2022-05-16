1160c6462ccb5114bbc2a383c23d26a6c40f4d2e
felipesurfe <felipe_surfe@hotmail.com>
2022-05-16 18:45:16 -0300

    add timer function

--- script.js
+++ script.js
@@ -11,16 +11,35 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
 
 //Event-driven
 //Programação imperativa
 //Callback 
+
+function countdown(){
+  setTimeout(function() {
+    let seconds = Number(secondsDisplay.textContent)
+
+    if( seconds <= 0 ) {
+      seconds = 60
+    }
+
+    secondsDisplay.textContent = seconds - 1
+    countdown()
+  }, 1000)
+}
 
 buttonPlay.addEventListener('click', function() {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countdown()
+
 })
 
 buttonPause.addEventListener('click', function() {
@@ -43,4 +62,8 @@ buttonSoundOn.addEventListener('click', function() {
 buttonSoundOff.addEventListener('click', function() {
   buttonSoundOff.classList.add('hide')
   buttonSoundOn.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function() {
+  minutes = prompt('Quantos minutos?')
 })
